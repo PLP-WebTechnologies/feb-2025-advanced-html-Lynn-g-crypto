429c56a22335cf0d081a9bb508898b4ac6daed9b
Lynn-g-crypto <mainalynn43@gmail.com>
2025-04-09 20:39:39 +0300

    initial commit

---
+++ List.py
@@ -0,0 +1,22 @@
+My_list = [10,20,30,40]
+print(My_list)
+
+My_list = [10,20,30,40]
+My_list.insert(2,15)
+print(My_list)
+
+My_list =[10,20,15,30,40]
+My_list.extend([50,60,70])
+print(My_list)
+
+My_list = [10,20,15,30,40,50,60,70]
+My_list = My_list[:-1]
+print(My_list)
+
+My_list = [10,20,15,30,40,50,60]
+My_list.sort()
+print(My_list)
+
+My_list = [10,20,15,30,40,50,60]
+My_list.index(30)
+print(My_list.index(30))
